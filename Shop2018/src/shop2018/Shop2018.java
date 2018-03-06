6969176e2ab06dd5326c92cb5b9ced9d7b8dd911
Student <Student@193.107.34.217>
2018-03-06 13:20:19 +0100

    muj pierwszy raz. struktura projektu

--- Shop2018/src/shop2018/Shop2018.java
+++ Shop2018/src/shop2018/Shop2018.java
@@ -5,6 +5,8 @@
  */
 package shop2018;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author Student
@@ -15,10 +17,10 @@ public class Shop2018 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     Produkt chleb = new Produkt("chleb",(2.2));
-     Produkt maslo = new Produkt("maslo",(6.15));
-     Produkt mleko = new Produkt("mleko",(1.9));
-     Produkt jogurt = new Produkt("jogurt",(0.95));
+     Produkt chleb = new Produkt("chleb",new BigDecimal("2.2"));
+     Produkt maslo = new Produkt("maslo",new BigDecimal("6.15"));
+     Produkt mleko = new Produkt("mleko",new BigDecimal("1.9"));
+     Produkt jogurt = new Produkt("jogurt",new BigDecimal("0.95"));
      
      Koszyk koszyk = new Koszyk();
      koszyk.dodaj(chleb);
